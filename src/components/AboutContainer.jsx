dd621e1645c42a9f7f53cc219d888f4cfc09b298
georgelacademico <george.lima@academico.ifpb.edu.br>
2023-01-09 15:31:45 -0300

    Preparando o deploy

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,7 +1,7 @@
 const AboutContainer = () => {
   return (
     <section className="about-container">
-        <h2>Sobre mim:</h2>
+        <h2>Sobre mim</h2>
         <p>Olá. Me chamo George e sou um <b>programador full-stack</b> em formação. Tenho 29 anos e moro em João Pessoa, mesma cidade onde também estudo no curso <b>tecnólogo em Sistemas para Internet</b> no IFPB.</p>
         <p>Possuo graduação anterior em Línguas Estrangeiras Aplicadas às Negociações Internacionais pela UFPB, curso este que me proporcionou o nível de <b>inglês fluente</b> além de espanhol e francês em níveis técnicos.</p>
         <p>Estou atualmente no terceiro período de TSI e <b>aberto para ofertas de estágio</b> nas áreas de programação e desenvolvimento web. Abaixo listo minhas competências atuais.</p>
--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectsContainer = () => {
   return (
     <section className="projects-container">
-        <h2>Projetos:</h2>
+        <h2>Projetos</h2>
         <p>Meus projetos atuais são desenvolvidos no contexto acadêmico para as disciplinas da faculdade e tanto seus códigos quanto versões de visualização estão disponíveis no meu GitHub, acessível através do botão abaixo.</p>
         <a href="https://github.com/GeorgeLimaDev?tab=repositories" className='btn'>
             Ver projetos
