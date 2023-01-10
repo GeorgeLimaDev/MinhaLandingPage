67805f1e78e26b59015d9d020cd252e0fbb084d3
georgelacademico <george.lima@academico.ifpb.edu.br>
2023-01-10 10:16:05 -0300

    Preparando deploy.

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,8 +3,8 @@ const AboutContainer = () => {
     <section className="about-container">
         <h2>Sobre mim</h2>
         <p>Olá. Me chamo George e sou um <b>programador full-stack</b> em formação. Tenho 29 anos e moro em João Pessoa, mesma cidade onde também estudo no curso <b>tecnólogo em Sistemas para Internet</b> no IFPB.</p>
-        <p>Possuo graduação anterior em Línguas Estrangeiras Aplicadas às Negociações Internacionais pela UFPB, curso este que me proporcionou o nível de <b>inglês fluente</b> além de espanhol e francês em níveis técnicos.</p>
-        <p>Estou atualmente no terceiro período de TSI e <b>aberto para ofertas de estágio</b> nas áreas de programação e desenvolvimento web. Abaixo listo minhas competências atuais.</p>
+        <p>Possuo graduação anterior em Línguas Estrangeiras Aplicadas às Negociações Internacionais pela UFPB. Curso este que me proporcionou o nível de <b>inglês fluente</b> além de espanhol e francês em níveis intermediários.</p>
+        <p>Estou atualmente no terceiro período de TSI e <b>aberto para ofertas de estágio</b> nas áreas de programação e desenvolvimento web. Abaixo listo minhas competências técnicas atuais.</p>
     </section>
   )
 }
